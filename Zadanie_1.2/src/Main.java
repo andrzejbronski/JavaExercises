8dc91f1e1c373f971f1c33d1e51030c4aacf3e71
Andrzej Broński <andbronski@gmail.com>
2019-01-31 18:04:13 +0100

    Added soultion for task 1.2

---
+++ Zadanie_1.2/src/Main.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[]args){
+        double pi = Math.PI;
+        System.out.println("Program wyświetlający wartość liczby PI zaokrąglonej do pięciu miejsc po przecinku");
+        System.out.printf("%.5f",pi);
+    }
+}

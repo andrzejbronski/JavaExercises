0f3155da81a120daec8251f56167ade934b48214
Andrzej Broński <andbronski@gmail.com>
2019-02-02 12:42:28 +0100

    Added solution for task 1.3

---
+++ Zadanie_1.3/src/Main.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Program pierwiastek kwadratowy z liczby PI");
+        System.out.print("Pierwiastek kwadratowy z liczby Pi wynosi: ");
+        System.out.printf("%4.2f",Math.sqrt(Math.PI));
+    }
+}

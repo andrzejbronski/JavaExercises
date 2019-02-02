5b174129ddce6f9539d3e8e50394c0a3364b854c
Andrzej Broński <andbronski@gmail.com>
2019-02-02 17:37:43 +0100

    Added solution for task 1.5

---
+++ Zadanie_1.5/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]args){
+//a=37, b=11
+        System.out.println("Program dzielenie liczb całkoitych bez reszty.");
+        int a = 37, b = 11;
+        System.out.println(a/b);
+    }
+}

748e5dda76e489abb51df3a34a2da0709487b326
Andrzej Broński <andbronski@gmail.com>
2019-02-02 17:42:53 +0100

    Added solution for task 1.6

---
+++ Zadanie_1.6/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args){
+        //a =37, b=11, oblicz resztę z dzielenia całkowitego
+
+        System.out.println("Program wyliczający resztę z dzielenia całkowitego");
+        int a = 37, b =11;
+        System.out.println("Reszta z dzielenia wynosi: "+a%b);
+
+    }
+}

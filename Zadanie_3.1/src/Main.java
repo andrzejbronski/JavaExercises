701b1c5e3d5812b9b8d748ecce7b7f8a0cdc6f91
Andrzej Broński <andbronski@gmail.com>
2019-02-13 11:30:14 +0100

    Added solution for task 3.1

---
+++ Zadanie_3.1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program pętla for\n");
+        System.out.println("Wartość wyrażenia y=3x wynosi:");
+
+            for (int i = 0; i <= 10; i++) {
+                System.out.println("dla x = "+i+" wyrażenie wynosi: "+(3*i));
+            }
+        System.out.println("Koniec programu");
+
+    }
+}
